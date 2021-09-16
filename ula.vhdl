c29da96260f2da7b5436dbd5de91212ebe1fa3ac
Erick Correia Silva <silvaerick331@gmail.com>
2021-09-16 14:13:35 -0300

    Update ula.vhdl

--- ula.vhdl
+++ ula.vhdl
@@ -6,7 +6,7 @@ use IEEE.std_logic_unsigned.all; --pacote para comandos de adição e subtraçã
 
 entity ula is port(
 
-	A,B	:	in std_logic_vector (3 downto 0);
+    A,B	:	in std_logic_vector (3 downto 0);
     O	:	out std_logic_vector (3 downto 0);
     sel :	in std_logic_vector (2 downto 0)
 
@@ -20,16 +20,17 @@ begin
 process(A, B, sel)
 begin 
 
-	case sel is
+     case sel is
     
     	when "000" => O <= A + B;
-		when "001" => O <= A - B;
+	when "001" => O <= A - B;
         when "010" => O <= A and B;
         when "011" => O <= A or B;
         when "100" => O <= A xor B;
         when "101" => O <= not A;
         when "110" => O <= not B;
         when others => O <= "ZZZZ";
+
     end case;
     
 end process;
